eaff2cd7e156da1d0bfcfa298a0586ce7b9aea3f
mohamedweal901 <mohamedweal901@gmail.com>
2026-01-09 21:23:03 +0200

    add view pager 2 and bottomnavigation for activity_main and create derawable file for select and un select color for bottomnavigation

---
+++ app/src/main/java/com/example/e_commerce/ui/home/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.e_commerce.ui.home
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.example.e_commerce.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+    }
+}
